118b9a8e6678770f3f811bb649e1a59adfe2472a
anea1976 <azam_aurelie@yahoo.com>
2021-03-20 19:24:04 -0300

    Total amount of points

---
+++ total-amount-of-points.js
@@ -0,0 +1,39 @@
+Codewars 8kyu: Total amount of points(Link: https://www.codewars.com/kata/5bb904724c47249b10000131/train/javascript )
+    Instructions:
+
+    Our football team finished the championship.The result of each match look like "x:y".Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship.Rules for counting points for each match:
+    if x > y - 3 points
+if x < y - 0 point
+if x = y - 1 point
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+
+SOLUTION:
+function points(games) {
+
+    let sum = 0;
+
+    games.map(game => {
+
+        if (game[0] === game[2]) {
+
+            sum += 1
+
+        } else if (game[0] > game[2]) {
+
+            sum += 3
+
+        }
+
+    });
+
+    return sum;
+
+}
