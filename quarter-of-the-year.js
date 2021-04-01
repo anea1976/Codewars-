d75ee586457ac55c6f2d2cf87042ccca2a901e36
aureliefomum <azam_aurelie@yahoo.com>
2021-04-01 18:22:23 -0300

    got the quarters of the year!

---
+++ quarter-of-the-year.js
@@ -0,0 +1,23 @@
+// Codewars 8kyu QUARTER OF THE YEAR
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+SOLUTION 1:
+const quarterOf = (month) => {
+    // Your code here
+    if (month >= 1 && month <= 3) {
+      return 1;
+    } else if (month >= 4 && month <= 6) {
+      return 2;
+    } else if (month >= 7 && month <= 9) {
+      return 3;
+    } else {
+      return 4;
+    }
+
+    SOLUTION 2
+    const quarterOf = (month) => {
+        return Math.ceil( month/3);
+      }
