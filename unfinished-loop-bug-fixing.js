ea4139f178cde9f0b427473cc15cc3754a75ece7
aureliefomum <azam_aurelie@yahoo.com>
2021-04-11 21:18:54 -0300

    Fixing Tommy's bug

---
+++ unfinished-loop-bug-fixing.js
@@ -0,0 +1,15 @@
+CODEWARS 8KYU UNFINISHED LOOP- BUG FIXING #1
+INSTRUCTIONS:
+Unfinished Loop - Bug Fixing #1
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+SOLUTION:
+function createArray(number){
+    var newArray = [];
+    
+    for(var counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
