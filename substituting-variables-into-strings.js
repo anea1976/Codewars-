3b873c3bc03e4a5c66629ef74f78c0a137756df9
aureliefomum <azam_aurelie@yahoo.com>
2021-04-24 18:55:32 -0300

    substituting variables into strings

---
+++ substituting-variables-into-strings.js
@@ -0,0 +1,12 @@
+// CODEWARS 7KYU Substituting Variables Into Strings: Padded Numbers
+// Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+// Example:
+
+// solution(5) // should return "Value is 00005"
+
+//SOLUTION
+
+function solution(value){
+    return `Value is ${value.toString().padStart(5, '0')}`
+  }
