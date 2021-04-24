e61ad4c50de4fe9e8a473d26fd585d5a208ffe3a
aureliefomum <azam_aurelie@yahoo.com>
2021-04-24 19:02:28 -0300

    sorting numbers

---
+++ sort-numbers.js
@@ -0,0 +1,20 @@
+// CODEWARS 7KYU : SORT NUMBERS
+// INSTRUCTIONS:
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+//SOLUTION:
+function solution(nums){
+    return (nums === null|| nums.length===0) ? [] :nums.sort((a, b) => a-b)  ;
+    }
+
+//SOLUTION 2:
+function solution(nums){
+    return (nums || []).sort(function(a, b){
+      return a - b
+    });
+  }
