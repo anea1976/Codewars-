c6809c5e5a69e12a41d591ffaea4022b1fa00c2f
aureliefomum <azam_aurelie@yahoo.com>
2021-05-05 16:57:20 -0300

    simple array sorting

---
+++ sort-arrays.js
@@ -0,0 +1,11 @@
+// CODEWARS 7KYU - SORT ARRAYS - 1
+
+// INSTRUCTIONS:
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list in a sorted manner.
+
+// SOLUTION:
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    return names.sort()
+  }
