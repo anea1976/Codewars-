53997b81c1af250737c2c7e5ee9aac2a56e5f543
aureliefomum <azam_aurelie@yahoo.com>
2021-05-05 17:06:02 -0300

    max min arrays

---
+++ max-min-arrays.js
@@ -0,0 +1,26 @@
+// CODEWARS 7KYU - MAX-MIN ARRAYS
+// INSTRUCTIONS:
+// In this Kata, you will be given an array of unique elements, and your task is to rearrange the values so that the first max value is followed by the first minimum, followed by second max value then second min value, etc.
+
+// For example:
+
+// solve([15,11,10,7,12]) = [15,7,12,10,11]
+// The first max is 15 and the first min is 7. The second max is 12 and the second min is 10 and so on.
+
+// More examples in the test cases.
+
+// Good luck!
+
+// SOLUTION:
+function solve(arr){
+    let  minMax = [];
+    arr.sort((a,b)=>a-b);
+    for(let i = 0, j = arr.length-1; i <= j; i++,j--){
+      if(i != j) minMax.push(arr[j]), minMax.push(arr[i]);
+      else minMax.push(arr[i]);
+    }
+    return minMax;
+  }
+  //BEST PRACTICE SOLUTION:
+  const solve = arr =>
+  [...Array(arr.sort((a, b) => a - b).length)].map((_, idx) => idx % 2 ? arr.shift() : arr.pop());
