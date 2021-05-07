95ad6adf52c2973a46672590ae650f7ba1b03ea7
aureliefomum <azam_aurelie@yahoo.com>
2021-05-07 20:16:08 -0300

    commented out instructions

--- sum-mixed-array.js
+++ sum-mixed-array.js
@@ -1,7 +1,7 @@
-Sum Mixed Array
-Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Sum Mixed Array
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
-Return your answer as a number.
+// Return your answer as a number.
 
     Solution:
 function sumMix(x) {
