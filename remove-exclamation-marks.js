a0c6291b8c494a3360b35db865e33115e9d0fa52
aureliefomum <azam_aurelie@yahoo.com>
2021-05-14 22:34:22 -0300

    remove exclamation marks

---
+++ remove-exclamation-marks.js
@@ -0,0 +1,11 @@
+// CODEWARS 8KYU -REMOVE ALL THE EXCLAMATION MARKS
+
+// INSTRUCTIONS:
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// SOLUTION:
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/g, '') ;
+  }
