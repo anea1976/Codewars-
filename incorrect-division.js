66cde3a236e26b26c4dce71ae6859268fe60fe99
aureliefomum <azam_aurelie@yahoo.com>
2021-05-29 19:41:59 -0300

    incorrect division

---
+++ incorrect-division.js
@@ -0,0 +1,9 @@
+// CODEWARS 8KYU - INCORRECT DIVISION 
+
+// INSTRUCTIONS: 
+// This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+// BUGS
+
+// SOLUTION:
+const solve = (x, y) => x / y
