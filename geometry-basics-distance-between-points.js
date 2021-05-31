e71aba58c8e51a0a2bf4e0257a19dc1ebe80d6a5
aureliefomum <azam_aurelie@yahoo.com>
2021-05-30 22:48:36 -0300

    goemetry basics distance between points

---
+++ geometry-basics-distance-between-points.js
@@ -0,0 +1,15 @@
+// CODEWARS 8KYU - GEOMETRY BASICS DISTANCE BETWEEN POINTS IN 2D 
+//  INSTRUCTIONS:
+//  This series of katas will introduce you to basics of doing geometry with computers.
+
+// Point objects have x and y attributes (X and Y in C#) attributes.
+
+// Write a function calculating distance between Point a and Point b.
+
+// Tests round answers to 6 decimal places.
+
+//SOLUTION:
+
+function distanceBetweenPoints(a, b) {
+    return Math.hypot(a.x-b.x, a.y-b.y)
+  }
