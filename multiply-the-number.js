ad686945937ebc6931e448b985aa847b72957568
aureliefomum <azam_aurelie@yahoo.com>
2021-06-10 23:35:16 -0300

    multiply the number

---
+++ multiply-the-number.js
@@ -0,0 +1,16 @@
+// CODEWARS 8KYU - MULTIPLY THE NUMBER
+//  INSTRUCTIONS:
+//  Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+// multiply(3)==15
+// multiply(10)==250
+// multiply(200)==25000
+// multiply(0)==0
+// multiply(-3)==-15
+
+// SOLUTION:
+
+function multiply(number){
+    let numOfDigits = number.toString().replace('-', '').length
+    return number*(5**numOfDigits)
+  }
