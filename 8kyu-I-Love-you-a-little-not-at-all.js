1e423831b850410132c688a48c39898d868d7de0
aureliefomum <azam_aurelie@yahoo.com>
2021-06-15 12:05:04 -0300

    LOVE YOU A LITTLE BIT, PASSIONATELY, NOT AT ALL

--- 8kyu-I-Love-you-a-little-not-at-all.js
+++ 8kyu-I-Love-you-a-little-not-at-all.js
@@ -19,3 +19,19 @@ function howMuchILoveYou(nbPetals) {
     let phrases = ['not at all','I love you','a little','a lot','passionately','madly']
     return phrases[nbPetals % 6]
 }
+// CLEVER SOLUTIONS:
+function howMuchILoveYou(nbPetals) {
+    let phrase = {    
+      0: "not at all",
+      1: "I love you",
+      2: "a little",
+      3: "a lot",
+      4: "passionately",
+      5: "madly"
+    }
+    return phrase[nbPetals%6]
+  }
+
+
+  const howMuchILoveYou = nbPetals =>
+  [`not at all`, `I love you`, `a little`, `a lot`, `passionately`, `madly`][nbPetals % 6];
