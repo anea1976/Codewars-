d1614e587269f679509549ebb4c1c86d75f4cd84
aureliefomum <azam_aurelie@yahoo.com>
2021-06-21 21:29:20 -0300

    variable assignment

---
+++ 8kyu-variable-assignment-debug.js
@@ -0,0 +1,12 @@
+// CODEWARS 8KYU - Grasshopper - Variable Assignment Debug
+// INSTRUCTIONS: 
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+// FUNDAMENTALS
+
+// SOLUTION:
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
