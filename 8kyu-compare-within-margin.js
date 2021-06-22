d8237fb770f9a95e67a95e87648e82d846b6e08d
aureliefomum <azam_aurelie@yahoo.com>
2021-06-22 16:34:42 -0300

    compare within margin

---
+++ 8kyu-compare-within-margin.js
@@ -0,0 +1,14 @@
+// CODEWARS 8KYU -COMPARE WITHIN MARGIN
+// INSTRUCTIONS 
+
+
+// SOLUTION:
+// If margin is not given, treat it as zero.
+
+// Example: if a = 3, b = 5 and the margin = 3, since a and b are no more than 3 apart, close_compare should return 0. Otherwise, if instead margin = 0, a is lower than b and close_compare should return -1.
+
+// Assume: margin >= 0
+
+// Tip: Some languages have a way to make arguments optional.
+
+// FUNDAMENTALS
