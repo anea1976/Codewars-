3c1e7f853a814274d68607792108e337628b6008
aureliefomum <azam_aurelie@yahoo.com>
2021-06-25 18:44:22 -0300

    grasshopper order of operations

---
+++ 8kyu-grasshopper-order-of-operations.js
@@ -0,0 +1,9 @@
+// CODEWARS 8KYU - GRASSHOPPER ORDER OF OPERATIONS 
+// INSTRUCTIONS 
+// Grasshopper Order of Operations
+// You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. Fix the function to make it return 32 without changing the number or the operators.
+
+// FUNDAMENTALS VARIABLES BASIC LANGUAGE FEATURES
+
+//SOLUTION:
+
