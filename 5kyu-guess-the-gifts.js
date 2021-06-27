d947ae6c55aa3a0e770a032f2873b53ab64e9a8d
aureliefomum <azam_aurelie@yahoo.com>
2021-06-27 09:28:15 -0300

    guess the gifts

--- 5kyu-guess-the-gifts.js
+++ 5kyu-guess-the-gifts.js
@@ -46,4 +46,23 @@ function guessGifts(wishlist, presents) {
   }
   return [...new Set(finalArr)]
 }
+//Other solutions:
+function guessGifts(wishlist, presents) {
+  return wishlist.filter(function(x){
+    return presents.some(function(y){
+      return x.size == y.size && x.clatters == y.clatters && x.weight == y.weight;
+    });
+  }).map(function(x){ return x.name; });
+}
+
+function guessGifts(wishlist, presents) {
+  var r = [];
+  presents.forEach(function(p){
+    wishlist.forEach(function(w){
+      if (p.size == w.size && p.clatters == w.clatters && p.weight == w.weight && r.indexOf(w.name) == -1) r.push(w.name);
+    });
+  });
+  return r;
+}
+
 
