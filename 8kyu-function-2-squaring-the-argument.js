f388aab5128996651c90a784ba2ca84a88848f5a
aureliefomum <azam_aurelie@yahoo.com>
2021-06-28 11:22:23 -0300

    squaring the argument

---
+++ 8kyu-function-2-squaring-the-argument.js
@@ -0,0 +1,8 @@
+// CODEWARS 8KYU - SQUARING AN ARGUMENT 
+// INSTRUCTIONS 
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// SOLUTION:
+function square(n){
+    return n**2
+  }
