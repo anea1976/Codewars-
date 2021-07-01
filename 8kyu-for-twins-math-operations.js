cc4430d13672ab3db2c2c84eec67ac7931fb02c9
aureliefomum <azam_aurelie@yahoo.com>
2021-07-01 18:18:55 -0300

    for twins math operations

--- 8kyu-for-twins-math-operations.js
+++ 8kyu-for-twins-math-operations.js
@@ -9,4 +9,9 @@
 // bottleLength - total bottle length (always > 0);
 // rimLength - length from bottle top to brick (always < bottleLength);
 // And return volume of ice brick that magician managed to put into a bottle.
+//SOLUTION:
+
+function iceBrickVolume(radius, bottleLength, rimLength) {
+    return (radius ** 2) * (bottleLength-rimLength) * 2
+  }
 
