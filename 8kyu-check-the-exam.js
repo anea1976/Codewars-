cf9ba13aa19e8d62ece4095f3b4da79324cb369d
aureliefomum <azam_aurelie@yahoo.com>
2021-07-05 15:30:02 -0300

    check the exam

--- 8kyu-check-the-exam.js
+++ 8kyu-check-the-exam.js
@@ -12,3 +12,18 @@
 // checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
 // checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
 // checkExam(["b", "c", "b", "a"], ["",  "a", "a", ", "c"]) → 0
+
+// SOLUTION
+function checkExam(array1, array2) {
+    let score = 0
+    for (let i=0; i<array1.length;i++){
+           if(array1[i]===array2[i]){
+          score+=4
+        }else if(array2[i]=== ""){
+          score+=0
+        }else{
+          score-=1
+        }  
+    }
+     return score < 0 ? 0: score;
+   }
