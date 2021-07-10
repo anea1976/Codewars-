a77348a31fcd719dbd6da4e0fe9b301078154271
aureliefomum <azam_aurelie@yahoo.com>
2021-07-10 16:38:11 -0300

    are they the same

--- 6kyu-are-they-the-same.js
+++ 6kyu-are-they-the-same.js
@@ -63,6 +63,13 @@ function comp(array1, array2){
   
   return !array1 || !array2 ||  array1.length !== array2.length ? false : array1.map(el => el**2).sort().toString()===array2.sort().toString();
 }
+//OTHER SOLUTIONS:
+function comp(array1, array2) {
+  if(array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
+  return array1.map(v => v * v).every((v, i) => v == array2[i]);
+}
 
+x
 
 
