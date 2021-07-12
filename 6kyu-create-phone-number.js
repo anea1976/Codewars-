9c04462c7404afbbab1ae9de07b0e12c24b08c91
aureliefomum <azam_aurelie@yahoo.com>
2021-07-12 12:20:07 -0300

    create phone number

--- 6kyu-create-phone-number.js
+++ 6kyu-create-phone-number.js
@@ -5,4 +5,12 @@
 // Example:
 // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns
 
+// SOLUTION:
+function createPhoneNumber(numbers){
+  let firstSet = numbers.slice(0,3).join('')
+   let secondSet = numbers.slice(3,6).join('')
+  let thirdSet = numbers.slice(6,10).join('')
+  return `(${firstSet}) ${secondSet}-${thirdSet}`
+}
+
 
