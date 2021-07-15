d817137e3d2e5963e264002a2a92fc2051838f26
aureliefomum <azam_aurelie@yahoo.com>
2021-07-15 11:22:10 -0300

    convert string to camel case

---
+++ 6kyu-convert-string-to-camel-case.js
@@ -0,0 +1,9 @@
+// 6KYU CODEWARS - CONVERT STRING TO CAMEL CASE 
+
+// INSTRUCTIONS 
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
