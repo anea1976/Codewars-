3d52af8076f8a459ed818ff948b344d0c16bcb74
aureliefomum <azam_aurelie@yahoo.com>
2021-07-15 11:23:29 -0300

    convert string to camel case

--- 6kyu-convert-string-to-camel-case.js
+++ 6kyu-convert-string-to-camel-case.js
@@ -7,3 +7,24 @@
 // "the-stealth-warrior" gets converted to "theStealthWarrior"
 // "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
 
+// SOLUTIONS
+// 1)
+function toCamelCase(str){
+  //account for  empty string
+    if (str==="") return ""
+    //split string into array based on separators '-' or '_'
+     let strArray= str.includes('-')? str.split('-'):str.includes('_')? str.split('_'):str.split('');
+     //check whether first word in array is capitalized
+     let checkFirstCaps =String(strArray[0]).charAt(0)+ strArray[0].slice(1)===String(strArray[0]).charAt(0).toUpperCase()+ strArray[0].slice(1)
+     // cut out first non-capitalized word
+     let firstNonCaps =strArray.slice(0,1)
+     // grab second part of array with non-capitalized first word and capitalize all words in it
+     let secondNonCaps =strArray.slice(1).map(word=> word[0].toUpperCase()+ word.slice(1))
+     //join first non-capitalized word to rest of already capitalized array
+     let joinedNonCaps = [...firstNonCaps,...secondNonCaps].join('')
+     
+     return checkFirstCaps? strArray.map(word => word[0].toUpperCase() + word.slice(1)).join(''): joinedNonCaps;
+     
+       }
+
+       
