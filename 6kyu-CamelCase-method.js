6ab9660e18f9bb5011007e6f8fff70e1d1c3004f
aureliefomum <azam_aurelie@yahoo.com>
2021-07-16 11:17:05 -0300

    CamelCase method

--- 6kyu-CamelCase-method.js
+++ 6kyu-CamelCase-method.js
@@ -7,4 +7,12 @@
 
 // "hello case".camelCase() => HelloCase
 // "camel case word".camelCase() => CamelCaseWord
+// SOLUTIONS:
+//1)
+String.prototype.camelCase=function(str){
+  
+  return this.split(' ').map(word => word.charAt().toUpperCase()+ word.slice(1)).join('')
+  
+}
+
 
