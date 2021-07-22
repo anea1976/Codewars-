a462a3fa4bba85f46aedecb3350d32aa979f1840
aureliefomum <azam_aurelie@yahoo.com>
2021-07-22 11:40:25 -0300

    break camel case

--- 6kyu-break-camel-case.js
+++ 6kyu-break-camel-case.js
@@ -13,15 +13,3 @@ function solution(string) {
     return string.split('').map(x=> x === x.toUpperCase()? ` ${x}`: x).join('')
 }
 
-//2)
-const solution = string => {
-    return [...string].map((char) => {
-      return (char === char.toUpperCase()) ? ` ${char}` : char;
-    }).join('');
-  }
-
-  // 3)
-
-  function solution(string) {
-    return string.replace(/[A-Z]/g, val => ' ' + val)
-}
