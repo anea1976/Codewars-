0f03825c104e4069aceed10b692f43e07bd56974
aureliefomum <azam_aurelie@yahoo.com>
2021-07-23 15:23:14 -0300

    sum of differences in array

--- 8kyus/8kyu-sum-of-differences-in-array.js
+++ 8kyus/8kyu-sum-of-differences-in-array.js
@@ -13,3 +13,8 @@
 
 // If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
  
+// SOLUTIONS
+// 1)
+function sumOfDifferences(arr) {
+    return arr.sort((a,b) => b-a).map((e,i, arr) => e - arr[i+1] ).slice(0,-1).reduce((a,b)=> a+b, 0)
+  }
