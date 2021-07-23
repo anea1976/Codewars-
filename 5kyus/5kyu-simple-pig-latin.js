f5f852fdd7d8eaae966152b4e31b5044c9c84874
aureliefomum <azam_aurelie@yahoo.com>
2021-07-23 14:50:19 -0300

    simple pig latin

---
+++ 5kyus/5kyu-simple-pig-latin.js
@@ -0,0 +1,10 @@
+// 5KYU CODEWARS - SIMPLE PIG LATIN 
+// INSTRUCTIONS 
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+
+// SOLUTIONS:
