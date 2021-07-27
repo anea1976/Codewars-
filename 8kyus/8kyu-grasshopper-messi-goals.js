9fdc87f012f174699109198f554e21e2029d09e8
aureliefomum <azam_aurelie@yahoo.com>
2021-07-27 18:24:57 -0300

    grasshopper messi goals

--- 8kyus/8kyu-grasshopper-messi-goals.js
+++ 8kyus/8kyu-grasshopper-messi-goals.js
@@ -18,3 +18,8 @@
 // Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
 
 //SOLUTION:
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
