61921e8f641b0e3e0b6cb64e42f9c8e9a3508f06
aureliefomum <azam_aurelie@yahoo.com>
2021-08-03 17:58:17 -0300

    find the capitals 2

--- 7kyus/7Kyu-find-the-capitals2.js
+++ 7kyus/7Kyu-find-the-capitals2.js
@@ -12,3 +12,12 @@ var capitals = function (word) {
 };
 
 // OTHERS
+// 2)
+var capitals = function (word) {
+  var caps = [];
+  for(var i = 0; i < word.length; i++) {
+    if(word[i].toUpperCase() == word[i]) caps.push(i);
+  }
+  return caps;
+};
+
