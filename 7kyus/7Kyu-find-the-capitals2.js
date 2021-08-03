742fa66725d802b93527376fe6c7dc9cac39ee0c
aureliefomum <azam_aurelie@yahoo.com>
2021-08-03 17:55:33 -0300

    find the capitals 2

---
+++ 7kyus/7Kyu-find-the-capitals2.js
@@ -0,0 +1,7 @@
+// 7KYU CODEWARS - FIND THE CAPITALS 2
+// INSTRUCTIONS
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
