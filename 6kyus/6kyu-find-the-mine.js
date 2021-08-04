22c89bf8d9c3b62721d6719bb3a11866f442dfe8
aureliefomum <azam_aurelie@yahoo.com>
2021-08-04 10:55:38 -0300

    find the mine

--- 6kyus/6kyu-find-the-mine.js
+++ 6kyus/6kyu-find-the-mine.js
@@ -20,3 +20,14 @@ function mineLocation(field){
   return [field.indexOf(...mineArray), ...mineIndex]
 }
 
+//2)
+function mineLocation(field){
+  for (var i = 0; i < field.length; i++) {
+    for (var j = 0; j < field[i].length; j++) {
+      if (field[i][j] === 1) {
+        return [i, j] 
+      }
+    }
+  }
+}
+
