e5869b90327a382a285e616a0e232286a214d22b
aureliefomum <azam_aurelie@yahoo.com>
2021-08-06 11:55:55 -0300

    double sort

--- 7kyus/7kyu-double-sort.js
+++ 7kyus/7kyu-double-sort.js
@@ -14,4 +14,10 @@ function dbSort(a){
   return [...numberSort, ...stringSort]
 }
 
+//2)
+function dbSort(a){
+  let num = a.filter(x => typeof x == 'number').sort((a,b) => a - b)
+  let string = a.filter(x => typeof x == 'string').sort()
+  return num.concat(string)
+}
 
