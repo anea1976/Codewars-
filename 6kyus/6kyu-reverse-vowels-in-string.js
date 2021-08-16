7cec98c8d62b21044fb4a631f82eff65d87da8a0
aureliefomum <azam_aurelie@yahoo.com>
2021-08-16 13:27:22 -0300

    reverse vowels in a string

---
+++ 6kyus/6kyu-reverse-vowels-in-string.js
@@ -0,0 +1,13 @@
+// 6KYU CODEWARS - REVERSE VOWELS IN A STRING
+
+
+
+// In this kata, your goal is to write a function which will reverse the vowels in a string. Any characters which are not vowels should remain in their original position. Here are some examples:
+
+// reverseVowels("Hello!"); // "Holle!"
+// reverseVowels("Tomatoes"); // "Temotaos"
+// reverseVowels("Reverse Vowels In A String") // "RivArsI Vewols en e Streng"
+// For simplicity, you can treat the letter y as a consonant, not a vowel.
+
+// Good luck!
+
