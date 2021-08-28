f15209b071b940a4f2d2b8ab437fb01af9cedcd8
aureliefomum <azam_aurelie@yahoo.com>
2021-08-28 14:20:48 -0300

    remove all exclamation marks from end of sentence

---
+++ 8kyus/8kyu-remove-all-exclamation-marks-at-end-of-sentence.js
@@ -0,0 +1,14 @@
+// 8KYU CODEWARS -Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+// INSTRUCTIONS
+// Description:
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+// FUNDAMENTALS
+
