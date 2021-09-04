015decf22d837c6cb725baccbc1cd94baf64c3a3
aureliefomum <azam_aurelie@yahoo.com>
2021-09-03 23:35:36 -0300

    grasshopper function syntax debugging

---
+++ 8kyus/8kyu-grasshopper-function-syntax-debugging.js
@@ -0,0 +1,10 @@
+// 8KYU CODEWARS -FUNCTION SYNTAX DEBUGGING
+
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+//SOLUTION
+
