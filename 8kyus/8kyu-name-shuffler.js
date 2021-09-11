ab285feb866c7f1e26f49716483a23fd0befa83b
aureliefomum <azam_aurelie@yahoo.com>
2021-09-11 18:15:03 -0300

    name shuffler

---
+++ 8kyus/8kyu-name-shuffler.js
@@ -0,0 +1,6 @@
+// 8KYU CODEWARS - NAME SHUFFLER 
+// INSTRUCTIONS:
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
