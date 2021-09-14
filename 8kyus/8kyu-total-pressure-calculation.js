d94c58d499d6e570a8b80a4ff35d79be712f5344
aureliefomum <azam_aurelie@yahoo.com>
2021-09-14 20:24:06 -0300

    total pressure calculation

--- 8kyus/8kyu-total-pressure-calculation.js
+++ 8kyus/8kyu-total-pressure-calculation.js
@@ -86,3 +86,11 @@
  
 
 // FUNDAMENTALS
+
+//SOLUTION:
+//1)
+solution= (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
+    let finalMass= (givenMass1/molarMass1)+(givenMass2/molarMass2)
+    let gasTemp = 0.082 * (temp + 273.15)
+    return (finalMass * gasTemp)/volume
+  }
