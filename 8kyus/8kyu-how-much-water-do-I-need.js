e5e8d4a290e7d1ec0e6764856f7f7e5b12fff50a
aureliefomum <azam_aurelie@yahoo.com>
2021-09-16 17:03:19 -0300

    how much water do  I need

--- 8kyus/8kyu-how-much-water-do-I-need.js
+++ 8kyus/8kyu-how-much-water-do-I-need.js
@@ -23,4 +23,9 @@ function howMuchWater(water, load, clothes){
   
 }
 
-
+//2)
+function howMuchWater(water, load, clothes){
+  if(clothes / load > 2) return "Too much clothes"
+  if(clothes < load) return "Not enough clothes"
+  return Number((water * 1.1**(clothes - load)).toFixed(2))
+}
