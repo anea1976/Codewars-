aa71b0db0b1912c7b807b59d9b9b4e1d4d40c979
aureliefomum <azam_aurelie@yahoo.com>
2021-09-18 16:27:50 -0300

    convert hash to array

---
+++ 7kyus/7kyu-convert-hash-to-array.js
@@ -0,0 +1,11 @@
+
+// 7KYU CODEWARS - CONVERT HASH TO AN ARRAY
+
+// Convert a hash into an array. Nothing more, Nothing less.
+
+//   {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+// should be converted into
+
+//   [["name", "Jeremy"], ["age", 24], ["role", "Software Engineer"]]
+// Good Luck!
+
