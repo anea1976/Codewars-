4996f3a04b6b43fa6ffbdd3343a5a4ca6e954595
aureliefomum <azam_aurelie@yahoo.com>
2021-09-19 15:08:45 -0300

    convert hash to array

--- 7kyus/7kyu-convert-hash-to-array.js
+++ 7kyus/7kyu-convert-hash-to-array.js
@@ -9,8 +9,19 @@
 //   [["name", "Jeremy"], ["age", 24], ["role", "Software Engineer"]]
 // Good Luck!
 
-// SOLUTION
+// SOLUTIONS
+//1)
+function convertHashToArray(hash){
+  let array=[]
+ for(let elem in hash){
+   array.push([elem,hash[elem]])
+   
+ }
+     return array.sort()                      
+}
 
+
+//2)
 function convertHashToArray(hash) {
     let arr = [];
     Object.keys(hash).sort().map(item => arr.push([item,hash[item]]));
