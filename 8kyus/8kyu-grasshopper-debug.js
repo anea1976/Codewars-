2a1436965d3349f0df109c68d8a692c2fc4bdf3d
aureliefomum <azam_aurelie@yahoo.com>
2021-09-21 16:07:47 -0300

    grasshopper bug

--- 8kyus/8kyu-grasshopper-debug.js
+++ 8kyus/8kyu-grasshopper-debug.js
@@ -10,3 +10,20 @@
 // Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
 
 // BUGS
+
+// SOLUTION:
+function convertToCelsius (temperature) {
+    var celsius = (temperature -32) * 5/9 
+    return celsius
+  }
+  
+  
+  
+  function weatherInfo (temp) {
+    let c = convertToCelsius(temp)
+    if (+c < 0)
+      return (c + " is freezing temperature")
+    else
+      return (c + " is above freezing temperature")
+  }
+  
