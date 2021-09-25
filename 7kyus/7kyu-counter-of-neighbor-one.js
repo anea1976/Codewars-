9016bff25065a0335d568d128ef20184a0d87a00
aurelie fomum <azam_aurelie@yahoo.com>
2021-09-25 16:43:06 -0300

    Merge branch 'master' of https://github.com/aureliefomum/Codewars-

---
+++ 7kyus/7kyu-counter-of-neighbor-one.js
@@ -0,0 +1,10 @@
+// CODEWARS 7KYU - COUNTER OF NEIGHBOR ONES
+//INSTRUCTIONS
+// Task
+// Tranform of input array of zeros and ones to array in which counts number of continuous ones. If there is none, return an empty array
+
+// Example
+// [1, 1, 1, 0, 1] -> [3,1]
+// [1, 1, 1, 1, 1] -> [5]
+// [0, 0, 0, 0, 0] -> []
+// FUNDAMENTALS ALGORITHMS
