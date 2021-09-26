a1ad140755db60785a497cec2e1101dd1e78096b
aureliefomum <azam_aurelie@yahoo.com>
2021-09-26 10:55:33 -0300

    welcome!

--- 8kyus/8kyu-welcome.js
+++ 8kyus/8kyu-welcome.js
@@ -52,3 +52,26 @@ function greet(language) {
       return 'Welcome'
     }
   }
+  //2)
+  function greet(language) {
+    var welcomes = {
+      english: 'Welcome',
+      czech: 'Vitejte',
+      danish: 'Velkomst',
+      dutch: 'Welkom',
+      estonian: 'Tere tulemast',
+      finnish: 'Tervetuloa',
+      flemish: 'Welgekomen',
+      french: 'Bienvenue',
+      german: 'Willkommen',
+      irish: 'Failte',
+      italian: 'Benvenuto',
+      latvian: 'Gaidits',
+      lithuanian: 'Laukiamas',
+      polish: 'Witamy',
+      spanish: 'Bienvenido',
+      swedish: 'Valkommen',
+      welsh: 'Croeso'
+    };
+    return language in welcomes ? welcomes[language] : welcomes.english;
+  };
