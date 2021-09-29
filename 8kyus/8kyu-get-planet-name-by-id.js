c1903a0e4842f8dfe22e439cb626d32e81233b08
aureliefomum <azam_aurelie@yahoo.com>
2021-09-28 21:23:22 -0300

    get planet name by ID

--- 8kyus/8kyu-get-planet-name-by-id.js
+++ 8kyus/8kyu-get-planet-name-by-id.js
@@ -36,3 +36,56 @@
     
 //     return name;
 //   }
+
+// SOLUTIONS:
+//1)
+function getPlanetName(id){
+        var name;
+        switch(id){
+          case 1:
+            name = 'Mercury'
+            break;
+          case 2:
+            name = 'Venus'
+            break;
+          case 3:
+            name = 'Earth'
+            break;
+          case 4:
+            name = 'Mars'
+            break;
+          case 5:
+            name = 'Jupiter'
+            break;
+          case 6:
+            name = 'Saturn'
+            break;
+          case 7:
+            name = 'Uranus'
+            break;
+          case 8:
+            name = 'Neptune'
+            break;
+        }
+        
+        return name;
+      }
+
+
+
+
+//2)
+function getPlanetName(id){
+    var name = {
+      1: 'Mercury',
+      2: 'Venus',
+      3: 'Earth',
+      4: 'Mars',
+      5: 'Jupiter',
+      6: 'Saturn',
+      7: 'Uranus',
+      8: 'Neptune',
+    };
+    
+    return name[id];
+  }
