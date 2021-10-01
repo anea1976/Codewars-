888593200b043ab0986a6c77f86a2e68b8ebdc4a
aureliefomum <azam_aurelie@yahoo.com>
2021-10-01 16:20:22 -0300

    define a card suit

---
+++ 8kyus/8kyu-define-a-card-suit.js
@@ -0,0 +1,15 @@
+// 8KYU CODEWARS -DEFINE A CARD SUIT 
+// INSTRUCTIONS 
+// You get any card as an argument. Your task is to return a suit of this card.
+
+// Our deck (is preloaded):
+
+// deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+//         '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+//         '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+//         '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+// FUNDAMENTALSSTRINGSBASIC LANGUAGE FEATURES
