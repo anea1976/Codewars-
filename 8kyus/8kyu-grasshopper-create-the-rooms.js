f877e5344a3b04e2e98fbaac22ee85cb7180ec22
aureliefomum <azam_aurelie@yahoo.com>
2021-10-05 16:40:15 -0300

    grasshopper create the rooms

--- 8kyus/8kyu-grasshopper-create-the-rooms.js
+++ 8kyus/8kyu-grasshopper-create-the-rooms.js
@@ -28,4 +28,16 @@ let rooms = {
    
  }
 
- 
+ //2)
+ class Room  {
+  constructor() {
+    this.name = '';
+    this.description = '';
+    this.completed= '';
+  }
+}
+var rooms = {
+  bedroom : new Room(),
+  kitchen : new Room(),
+  bathroom : new Room(),
+}
