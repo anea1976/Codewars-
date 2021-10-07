87ae202fa786eedd38af4618780ca1137015ca37
aureliefomum <azam_aurelie@yahoo.com>
2021-10-07 14:46:03 -0300

    removing elements

--- 8kyus/8kyu-removing-elements.js
+++ 8kyus/8kyu-removing-elements.js
@@ -14,5 +14,12 @@
 function removeEveryOther(arr){
   return arr.filter((_,i)=> i % 2 === 0)
 }
-
+//2)
+function removeEveryOther(arr){
+  var newArr=[];
+for (var i = 0; i < arr.length; i+=2){
+  newArr.push(arr[i]);
+  }
+return newArr;
+}
 
