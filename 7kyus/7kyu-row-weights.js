791a6143b21820e30f32786ad2c2c17801941f15
aureliefomum <azam_aurelie@yahoo.com>
2021-10-13 10:44:09 -0300

    row weights

---
+++ 7kyus/7kyu-row-weights.js
@@ -0,0 +1,12 @@
+// 7KYU CODEWARS - ROW WEIGHTS 
+// INSTRUCTIONS 
+
+// Scenario
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+// Task
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+
+  }
