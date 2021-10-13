8325be33571ab22acf7d6380902b4055ea6ea5d5
aureliefomum <azam_aurelie@yahoo.com>
2021-10-13 10:46:59 -0300

    row weights

--- 7kyus/7kyu-row-weights.js
+++ 7kyus/7kyu-row-weights.js
@@ -17,3 +17,11 @@ function rowWeights(array){
   return [num1,num2]
 }
 
+//2)
+function rowWeights(array){
+  var arr = [0, 0];
+  for (var i = 0; i < array.length; i++) {
+    i % 2 == 0 ? arr[0] += array[i] : arr[1] += array[i];
+  }
+  return arr;
+}
