f3fa7718a5d90137b098894861a68d4261d2416b
aureliefomum <azam_aurelie@yahoo.com>
2021-10-13 10:45:38 -0300

    row weights

--- 7kyus/7kyu-row-weights.js
+++ 7kyus/7kyu-row-weights.js
@@ -9,4 +9,11 @@
 // Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
 
 
-  }
+  // SOLUTIONS
+//1)
+function rowWeights(array){
+  let num1 =array.filter((el,i)=> i % 2 ===0  ).reduce((a,b)=> a+b,0)
+  let num2 = array.filter((el,i)=> i % 2 !==0  ).reduce((a,b)=> a+b,0)
+  return [num1,num2]
+}
+
