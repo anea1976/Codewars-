56504a9f6bbb983148cea90af778c41aac967af5
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-10-19 16:41:55 -0300

    even numbers in an array

--- 7kyus/7kyu-even-numbers-in-an-array.js
+++ 7kyus/7kyu-even-numbers-in-an-array.js
@@ -8,3 +8,8 @@
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 // FUNDAMENTALS ARRAYS NUMBERS BASIC LANGUAGE FEATURES
+
+//SOLUTION:
+function evenNumbers(array, number) {
+    return array.filter(num=> num % 2 === 0).slice(-number)
+  }
