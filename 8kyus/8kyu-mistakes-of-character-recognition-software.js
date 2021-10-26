0f6cf7e36ea0a423e09f033a9468c6569ea641b7
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-10-26 16:59:23 -0300

    mistakes of character recognition software

--- 8kyus/8kyu-mistakes-of-character-recognition-software.js
+++ 8kyus/8kyu-mistakes-of-character-recognition-software.js
@@ -26,4 +26,8 @@ function correct(string){
   return string.split('').map(char => correctChars[char]? correctChars[char]: char).join('')
  }
 
- 
+ //2)
+ function correct(string)
+{
+return string.replace(/5/g,"S").replace(/0/g,"O").replace(/1/g,"I");
+}
