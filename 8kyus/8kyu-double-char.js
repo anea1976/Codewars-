641892c4331d64410d00769f9f76ec0e3aebb836
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-11-09 12:07:14 -0300

    double char

---
+++ 8kyus/8kyu-double-char.js
@@ -0,0 +1,14 @@
+// 8KYU CODEWARS -  DOUBLE CHAR
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// doubleChar("String") ==> "SSttrriinngg"
+
+// doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+// doubleChar("1234!_ ") ==> "11223344!!__  "
+// Good Luck!
+
+// FUNDAMENTALS LOOPS CONTROL FLOWBASIC LANGUAGE FEATURES
+
+
+  
