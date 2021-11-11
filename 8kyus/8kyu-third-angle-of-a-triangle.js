01179e54924241dad749ade5db8b297e3367572e
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-11-11 15:34:08 -0300

    third angle of a triangle

---
+++ 8kyus/8kyu-third-angle-of-a-triangle.js
@@ -0,0 +1,13 @@
+// 8KYU CODEWARS - THIRD ANGLE OF A TRIANGLE
+
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+// FUNDAMENTALS
+
+//SOLUTION:
