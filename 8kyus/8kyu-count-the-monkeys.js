ce49f3a05511b9ca16d0e69a4a91d79e226d3714
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-11-29 14:36:55 -0300

    count the monkeys

--- 8kyus/8kyu-count-the-monkeys.js
+++ 8kyus/8kyu-count-the-monkeys.js
@@ -9,4 +9,10 @@
 // monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // monkeyCount(1) // --> [1]
 
+//SOLUTION:
+//1)
+function monkeyCount(n) {
+    return Array(n).fill().map((x,i) => i+1)
+   }
 
+   
