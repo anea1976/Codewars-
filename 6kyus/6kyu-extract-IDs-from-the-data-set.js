86df2fc0b12ca78215378534f8e05cdfb9422eca
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-11-30 16:32:11 -0300

    extract ids from data set

---
+++ 6kyus/6kyu-extract-IDs-from-the-data-set.js
@@ -0,0 +1,17 @@
+// 6KYU CODEWARS - EXTRACT THE IDS FROM THE DATASET 
+// INSTRUCTIONS: 
+// Complete the method so that it returns an array of all ID's passed in. The data structure will be similar to the following:
+
+// var data = {
+//   id: 1,
+//   items: [
+//     {id: 2},
+//     {id: 3, items: [
+//       {id: 4},
+//       {id: 5}
+//     ]}
+//   ]
+// }
+
+// extractIds(data) // should return [1,2,3,4,5]
+ //SOLUTIONS:
