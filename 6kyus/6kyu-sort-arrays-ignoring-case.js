8a92415dc78fa04c5f8a26d2900642ac623053a5
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-09 21:28:57 -0300

    sort arrays ignoring case

---
+++ 6kyus/6kyu-sort-arrays-ignoring-case.js
@@ -0,0 +1,9 @@
+// 6KYU CODEWARS - SORT ARRAYS (IGNORING CASE) 
+// INSTRUCTIONS: 
+
+// Sort the given array of strings in alphabetical order, case insensitive. 
+// For example:
+// ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+// ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+// FUNDAMENTALS ARRAYS SORTING ALGORITHMS
+
