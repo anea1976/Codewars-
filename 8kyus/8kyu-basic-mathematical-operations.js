5b1c46a972158010dff4bb67427975306a614b2d
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-15 14:46:02 -0300

    basic mathematical operations

--- 8kyus/8kyu-basic-mathematical-operations.js
+++ 8kyus/8kyu-basic-mathematical-operations.js
@@ -21,4 +21,13 @@ function basicOp(operation, value1, value2){
   
   }
 
-  
+  //2)
+  function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+': return value1 + value2;
+        case '-': return value1 - value2;
+        case '*': return value1 * value2;
+        case '/': return value1 / value2;
+        default: return 'Operation must be one of + - * /';
+    }
+}
