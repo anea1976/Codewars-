0b38d1da3422f5bd7bb826a321b16eb6c7618cd4
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-16 20:44:20 -0300

    did she say hallo

---
+++ 8kyus/8kyu-did-she-say-hallo.js
@@ -0,0 +1,16 @@
+// 8KYU CODEWARS - DID SHE SAY HALLO?
+// INSTRUCTIONS:
+// These are the languages of the possible people you met the night before:
+
+// hello - english
+// ciao - italian
+// salut - french
+// hallo - german
+// hola - spanish
+// ahoj - czech republic
+// czesc - polish
+// Notes
+
+// you can assume the input is a string.
+// to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
+// function should be case insensitive to pass the tests
