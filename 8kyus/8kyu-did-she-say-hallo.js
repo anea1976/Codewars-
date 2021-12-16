7e27669c07c7a17fcfeecafdb4607f91a02fc0e9
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-16 20:45:48 -0300

    did she say hallo

--- 8kyus/8kyu-did-she-say-hallo.js
+++ 8kyus/8kyu-did-she-say-hallo.js
@@ -14,3 +14,11 @@
 // you can assume the input is a string.
 // to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
 // function should be case insensitive to pass the tests
+
+// SOLUTION
+//1)
+function validateHello(greetings) {  
+    return /(hello|ciao|salut|hallo|hola|ahoj|czesc)/i.test(greetings)
+ }
+
+ 
