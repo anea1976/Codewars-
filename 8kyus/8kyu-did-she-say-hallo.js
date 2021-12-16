7d29622d2c4761f3f0ffea7b0524f30654eef59d
Aurelie Fomum <azam_aurelie@yahoo.com>
2021-12-16 20:46:35 -0300

    did she say hallo

--- 8kyus/8kyu-did-she-say-hallo.js
+++ 8kyus/8kyu-did-she-say-hallo.js
@@ -21,4 +21,22 @@ function validateHello(greetings) {
     return /(hello|ciao|salut|hallo|hola|ahoj|czesc)/i.test(greetings)
  }
 
- 
+ //2)
+ const hellos = {
+  hello: 'english',
+  ciao: 'italian',
+  salut: 'french',
+  hallo: 'german',
+  hola: 'spanish',
+  ahoj: 'czech republic',
+  czesc: 'polish',
+  }
+  
+  const validateHello = greetings => {
+    for (key in hellos) {
+     if (greetings.toLowerCase().includes(key)) {
+       return true
+     }
+    }
+    return false
+  }
