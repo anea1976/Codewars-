548ea0173739da71c230522d7963b6b067fe283b
aureliefomum <azam_aurelie@yahoo.com>
2021-12-30 15:22:22 -0300

    noobcode1 supersize me

---
+++ 8kyus/8kyu-noobcode01-supersize-me.js
@@ -0,0 +1,10 @@
+// 8KYU CODEWARS -NOOBCODE 01:SUPERSIZE ME...OR RATHER, THIS INTEGER!
+// INSTRUCTIONS
+// Write a function that rearranges an integer into its largest possible value.
+
+// Example (Input --> Output)
+
+// 123456 --> 654321
+// 105 --> 510
+// 12 --> 21
+// If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
